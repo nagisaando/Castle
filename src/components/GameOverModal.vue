f85eb11ce207c84633e09e9ea79550c38a261e21
Nagisa Ando <amnot.nobig.deal@gmail.com>
2025-07-26 14:53:06 -0600

    style: add mobile design

--- src/components/GameOverModal.vue
+++ src/components/GameOverModal.vue
@@ -100,10 +100,10 @@ function restartGame() {
 
 <template>
   <div v-if="show" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full text-center">
-    <div class="bg-[#f1e8e0] py-8 px-15 w-4/5 mx-auto rounded-2xl">
+    <div class="bg-[#f1e8e0] py-4 px-8 md:py-8 md:px-15 w-4/5 mx-auto rounded-2xl">
       <div>
-        <p v-if="bestScore" class="text-amber-500 text-4xl font-bold">NEW HIGH SCORE</p>
-        <p class="text-3xl mb-2 text-stone-800 font-bold">Your score: {{ distance }}
+        <p v-if="bestScore" class="text-amber-500 text-2xl md:text-4xl font-bold">NEW HIGH SCORE</p>
+        <p class="text-xl md:text-3xl mb-2 text-stone-800 font-bold">Your score: {{ distance }}
         </p>
         <p class="mb-2 text-sm text-stone-800 font-medium">Top 10 Paw Dodgers</p>
         <ul ref="leaderBoardListEL"
@@ -124,19 +124,19 @@ function restartGame() {
 
       <div v-if="!showForm" class="flex justify-center mt-8 text-2xl">
         <button v-if="bestScore && !submitted" @click="handleShowForm"
-          class="bg-amber-500 border-transparent font-medium w-[362px] mr-4 hover:-translate-y-0.5 cursor-pointer rounded px-6 py-2 text-base ">Leave
+          class="bg-amber-500 border-transparent font-medium w-[362px] mr-4 hover:-translate-y-0.5 cursor-pointer rounded px-6 py-2 text-sm md:text-base ">Leave
           your
           legacy</button>
         <button @click="restartGame"
-          class=" bg-stone-800 border-transparent font-medium w-[362px] hover:-translate-y-0.5 cursor-pointer rounded px-6 py-2 text-base">try
+          class=" bg-stone-800 border-transparent font-medium w-[362px] hover:-translate-y-0.5 cursor-pointer rounded px-6 py-2 text-sm md:text-base">try
           again</button>
       </div>
       <div v-else="showForm" class="flex justify-center mt-8 text-2xl">
         <button v-if="bestScore" @click="handleSubmit"
-          class="bg-amber-500 border-transparent font-medium w-[362px] mr-4 hover:-translate-y-0.5 cursor-pointer rounded px-6 py-2 text-base">
+          class="bg-amber-500 border-transparent font-medium w-[362px] mr-4 hover:-translate-y-0.5 cursor-pointer rounded px-6 py-2 text-sm md:text-base">
           Submit</button>
         <button @click="showForm = false"
-          class=" bg-stone-800 border-transparent font-medium w-[362px] hover:-translate-y-0.5 cursor-pointer rounded px-6 py-2 text-base">
+          class=" bg-stone-800 border-transparent font-medium w-[362px] hover:-translate-y-0.5 cursor-pointer rounded px-6 py-2 text-sm md:text-base">
           Cancel</button>
       </div>
     </div>
