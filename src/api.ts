1e52e3518686d8156d67a62642bbb7e44436b966
Nagisa Ando <amnot.nobig.deal@gmail.com>
2025-08-26 19:54:47 -0600

    add headers

--- src/api.ts
+++ src/api.ts
@@ -2,8 +2,11 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
-  timeout: 1000,
+  timeout: 10000,
   withCredentials: true,
+  headers: {
+    'x-api-key': 'H4oJvBoI6G2Q7fNDJzy8eaSxwG7qCgTacLbqYHR0'
+  }
 });
 
 export interface LeaderboardEntry {
